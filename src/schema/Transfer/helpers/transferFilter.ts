60c01426846c487f3952fabd254f6b11ed7e88da
Yevhen Kyriukha <kirgene@gmail.com>
2021-12-02 22:10:38 +0200

    Update packages.

--- src/schema/Transfer/helpers/transferFilter.ts
+++ src/schema/Transfer/helpers/transferFilter.ts
@@ -1,3 +1,13 @@
+/**************************************************************************
+ *  (C) Copyright Mojaloop Foundation 2020                                *
+ *                                                                        *
+ *  This file is made available under the terms of the license agreement  *
+ *  specified in the corresponding source code repository.                *
+ *                                                                        *
+ *  ORIGINAL AUTHOR:                                                      *
+ *       Yevhen Kyriukha <yevhen.kyriukha@modusbox.com>                   *
+ **************************************************************************/
+
 import { createPartyFilter } from '.';
 import { createParticipantFilter } from '.';
 import { NexusGenInputs } from 'nexus-typegen';
