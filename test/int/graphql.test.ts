a2a51558c4d5fe9ba1cb66c43fb9ab1df8b81086
Nguni Phakela <nguni52@gmail.com>
2024-11-05 11:02:59 +0200

    Add skeleton for integration tests.

--- test/int/graphql.test.ts
+++ test/int/graphql.test.ts
@@ -1,11 +1,10 @@
-// TransferSummary.test.ts
+// graphql.test.ts
 import { ObjectId } from 'mongodb';
 import { getMongoClient, Collection, closeMongoClientConnection } from '../../src/lib/mongo';
-import TransferSummary from '../../src/schema//TransferSummary';
 import { objectType, extendType, queryField } from 'nexus';
 const testTransfers = require('./data/reporting.transfers.json');
 
-describe('TransferSummary Integration Tests', () => {
+describe('GraphQL Integration Tests', () => {
     const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
     let collection: Collection;
 
@@ -34,18 +33,10 @@ describe('TransferSummary Integration Tests', () => {
         }
     });
 
-    describe('insert data into live MongoDB', () => {
-        it('should insert transfer summary data', async () => {
-            // Insert test data
-            const result = await collection.insertMany(testTransfers);
-
-            const insertedIds = result.insertedIds;
-
-            // get inserted ids count
-            const insertedIdsCount = Object.keys(insertedIds).length;
-
-            // Assert
-            expect(insertedIdsCount).toEqual(8);
+    describe('Send sample graphql queries', () => {
+        it('should fetch a transfer using 4 fields', async () => {
+            console.log("I am not seeing handlers and their respective graphql methods.");
+            
         });
     });
 });
