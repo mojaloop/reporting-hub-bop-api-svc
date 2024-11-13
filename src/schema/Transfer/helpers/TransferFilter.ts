2836801625189a53e7738595a893bd7a2044a6cc
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2024-11-13 10:24:47 +0530

    add: transfer filter to transfer resource
    - Added helper functions to generate dynamic where condition based on filter input.
    - Added filter to transfer resource
    - Rename query name transfers and getAllTransfers to transfer and transfers

---
+++ src/schema/Transfer/helpers/TransferFilter.ts
@@ -0,0 +1,64 @@
+export const createWhereCondition = (filter: any) => {
+    const whereCondition: any = {
+      createdAt: {},
+      payerParty: { is: {} },
+      payeeParty: { is: {} },
+      conversions: { some: {} },
+    };
+  
+    if (filter?.startDate) {
+      whereCondition.createdAt = {
+        gte: new Date(filter.startDate),
+      };
+    }
+  
+    if (filter?.endDate) {
+      whereCondition.createdAt = {
+        ...whereCondition.createdAt,
+        lte: new Date(filter.endDate),
+      };
+    }
+    if (filter?.errorCode !== undefined) {
+      whereCondition.errorCode = filter.errorCode;
+    }
+    if (filter?.transferSettlementWindowId !== undefined) {
+      whereCondition.transferSettlementWindowId = filter.transferSettlementWindowId;
+    }
+    if (filter?.transferState) {
+      whereCondition.transferState = filter.transferState;
+    }
+    if (filter?.conversionState) {
+      whereCondition.conversions.some.conversionState = filter.conversionState;
+    }
+    if (filter?.transactionType) {
+      whereCondition.transactionType = filter.transactionType;
+    }
+    if (filter?.sourceCurrency) {
+      whereCondition.sourceCurrency = filter.sourceCurrency;
+    }
+    if (filter?.targetCurrency) {
+      whereCondition.targetCurrency = filter.targetCurrency;
+    }
+  
+    if (filter?.payer) {
+      if (filter.payer.partyIdType) {
+        whereCondition.payerParty.is.partyIdType = filter.payer.partyIdType;
+      }
+  
+      if (filter.payer.partyIdentifier) {
+        whereCondition.payerParty.is.partyIdentifier = filter.payer.partyIdentifier;
+      }
+    }
+  
+    if (filter?.payee) {
+      if (filter.payee.partyIdType) {
+        whereCondition.payeeParty.is.partyIdType = filter.payee.partyIdType;
+      }
+  
+      if (filter.payee.partyIdentifier) {
+        whereCondition.payeeParty.is.partyIdentifier = filter.payee.partyIdentifier;
+      }
+    }
+  
+    return whereCondition;
+  };
